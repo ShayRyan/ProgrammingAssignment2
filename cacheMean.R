505442f830ebae0db0dd52a0949730615b60062c
ShayRyan <shay.ryan@outlook.com>
2014-08-19 00:24:38 +0100

    Working version
    Working but still some comments to include

---
+++ cacheMean.R
@@ -0,0 +1,42 @@
+# makeVector creates a special "vector", which is really a 
+#list containing a function to:
+# - set the value of the vector
+# - get the value of the vector
+# - set the value of the mean
+# - get the value of the mean
+
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    # return the original vector x
+    get <- function() {
+        x
+    }
+    setmean <- function(mean) {
+        m <<- mean
+    }
+    getmean <- function() {
+        m
+    }
+    list(set = set, get = get, setmean = setmean, getmean = getmean)
+}
+
+# calculate the mean of the special "vector" created with makeVector
+# First checks to see if the mean has already been calculated.
+# If so, take the mean from the cache otherwise, calculates the mean 
+# of the data and sets the value of the mean in the cache via the setmean function.
+
+cachemean <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,44 @@
 
 ## Write a short comment describing this function
 
-## change to test first commit
-
 makeCacheMatrix <- function(x = matrix()) {
-
+    # inverse is initially set to NULL
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    # return the original vector x
+    get <- function() {
+        x
+    }
+    
+    # called by cacheSolve once. Inverse matrix stored in inv
+    setinv <- function(solve) {
+        inv <<- solve
+    }
+    
+    # return the cached inverse matrix to cacheSolve
+    getinv <- function() {
+        inv
+    }
+    
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv<- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
